99fb28280bdb059d82671614fe4c599be5ad35af
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2025-02-26 21:04:15 +0700

    Сделан листинг новостей (данные находятся в json-файле в самом проекте) - добавлена разметка карточки и всего листинга

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
         <PageHeader />
+        <ListingArticles />
     </div>
 </template>
 
 <script>
 import PageHeader from "@/components/blocks/PageHeader.vue";
+import ListingArticles from "./components/blocks/ListingArticles.vue";
 
 export default {
     components: {
         PageHeader,
+        ListingArticles,
     },
 };
 </script>
---
+++ vue-project/src/components/blocks/ListingArticles.vue
@@ -0,0 +1,42 @@
+<template>
+    <main class="list-articles">
+        <div class="list-articles__container">
+            <div class="list-articles__list">
+                <CardArticle
+                    v-for="item in news"
+                    :key="item.id"
+                    :title="item.title"
+                    :description="item.description"
+                    class="list-articles__card"
+                />
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+import CardArticle from "./CardArticle.vue";
+import news from "@/json/news.json";
+
+export default {
+    components: {
+        CardArticle,
+    },
+    data() {
+        return {
+            news: news,
+        };
+    },
+};
+</script>
+
+<style lang="less">
+.list-articles {
+    &__container {
+        .container();
+    }
+    &__card {
+        margin-bottom: 15px;
+    }
+}
+</style>
